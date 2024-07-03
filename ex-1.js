c6fa654a8b2dc1bf2e27662811fb45f38b7c959f
paramee <huhaliza@gmail.com>
2024-07-03 23:33:04 +0700

    finished

--- ex-1.js
+++ ex-1.js
@@ -2,11 +2,20 @@
 
 function forEach(array, operation) {
   // Start coding here
+  for (let i in array) {
+    newEmployeeSalaries.push(operation(array[i]));
+  }
+}
+
+function increase(salary) {
+  return salary + 5000;
 }
 
 const employeeSalaries = [20005, 40000, 32000, 14500, 344000];
 const newEmployeeSalaries = [];
 
 // Using `forEach` function here
+
+forEach(employeeSalaries, increase);
 
 console.log(newEmployeeSalaries); // [25005, 45000, 37000, 19500, 349000]
